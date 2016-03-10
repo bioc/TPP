a5d01ad9cb8ba6e475a5dee5d75ff2b9e855c0fc
Dorothee Childs <dorothee.childs@embl.de>
2016-03-10 08:56:02 +0000

    Bugfixes and small adaptations so that R CMD check runs again without warnings.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_2/madman/Rpacks/TPP@114593 bc3139a8-67e5-0310-9ffc-ced21a209358

--- tests/testthat/test_normalization_functions.R
+++ tests/testthat/test_normalization_functions.R
@@ -1,8 +1,8 @@
 ## ------------------------------------------------------------------------- ##
 ## function 'applyCoeffs':
 ## ------------------------------------------------------------------------- ##
-test_that(desc="applyCoeffs", code={
-  message("Checking function 'applyCoeffs'")
+test_that(desc="testApplyCoeffs", code={
+  cat("Checking function 'applyCoeffs': ")
   x <- matrix(rnorm(10), nrow=2, ncol=5)
   eSet <- ExpressionSet(assayData=x)
 
@@ -11,5 +11,5 @@ test_that(desc="applyCoeffs", code={
   
   xNew <- unname(exprs(eSetNormed))
   xRef <- rbind(x[1,] * normCoeffs, x[2,] * normCoeffs)
-  expect_equal(xNew, xRef)
+  cat(paste("   ", expect_equal(xNew, xRef)))
 })
